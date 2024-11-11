a27fce6af35ba0eb1e0720685176bd1d2cf8f094
Agustin <agusrugbyy@gmail.com>
2024-11-11 19:37:25 -0300

    Notebook

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import __dirname from './utils.js'
 import { router as viewsRouter} from './routes/viewsRouter.js';
+import {engine} from 'express-handlebars'
 const PORT=3000;
 
 const app=express();
@@ -7,6 +9,17 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'))
+        
+//Handlebars configuracion
+app.engine('handlebars', engine({
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    },
+}))
+app.set('view engine', 'handlebars')//setea el motor de vistas
+app.set('views',  __dirname + '/views')//donde se encontraran las vistas
+
 
 app.use('/', viewsRouter)
 
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -3,11 +3,53 @@ export const router=Router()
 
 router.get('/',(req,res)=>{
 
-    res.status(200).json({ok: 'ok'})
+    res.status(200).render('home')
 })
 
+
+const productos = [
+    {
+      id: 1,
+      precio: 25.99,
+      title: "Camiseta",
+      descripcion: "Camiseta de algodón 100% en varios colores.",
+      stock: 50
+    },
+    {
+      id: 2,
+      precio: 49.99,
+      title: "Pantalón",
+      descripcion: "Pantalón de mezclilla con diseño clásico.",
+      stock: 30
+    },
+    {
+      id: 3,
+      precio: 15.99,
+      title: "Gorra",
+      descripcion: "Gorra ajustable en varios colores.",
+      stock: 100
+    },
+    {
+      id: 4,
+      precio: 120.0,
+      title: "Chaqueta",
+      descripcion: "Chaqueta de cuero sintético, ideal para el invierno.",
+      stock: 10
+    },
+    {
+      id: 5,
+      precio: 9.99,
+      title: "Calcetines",
+      descripcion: "Pack de calcetines de algodón.",
+      stock: 200
+    }
+  ];
+  
+
+
+
 router.get('/products', async (req,res) => {
-    return res.status(200).json({ok: 'Get Products'});
+    return res.status(200).render('products', {productos});
 })
 
 router.get('/products/:id', async ( req, res) => {
